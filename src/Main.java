8ee306123c5e3998e028b345777ee767ab185a68
Jdavid77 <jdavid72009@hotmail.com>
2021-10-23 18:51:14 +0100

    created Department

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Entities.Department;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
+    }
+}
